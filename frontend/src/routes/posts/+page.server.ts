02ab4bd36bbf39f16d7891b476bdbaa5970edfcd
Philipp Schardt <philipp.schardt@ergosign.de>
2023-06-03 20:45:47 +0200

    request with strapi api token

--- frontend/src/routes/posts/+page.server.ts
+++ frontend/src/routes/posts/+page.server.ts
@@ -1,7 +1,13 @@
+import { STRAPI_API_TOKEN } from '$env/static/private';
 import type { PageServerLoad } from './$types';
+
 
 export const load = (async ({ fetch }) => {
 	return {
-		posts: await (await fetch('http://localhost:1337/api/posts?populate=*')).json()
+		posts: await (await fetch('http://localhost:1337/api/posts?populate=*', {
+			headers: {
+				'Authorization': `bearer ${STRAPI_API_TOKEN}`
+			}
+		})).json()
 	};
 }) satisfies PageServerLoad;
