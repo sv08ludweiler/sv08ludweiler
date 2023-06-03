cbcced8c161a1c4ce772054ec81d2112fa93e9fd
Philipp Schardt <philipp.schardt@ergosign.de>
2023-06-03 21:25:02 +0200

    added navigation plugin

---
+++ frontend/src/routes/+layout.server.ts
@@ -0,0 +1,12 @@
+import { STRAPI_API_TOKEN } from '$env/static/private';
+import type { LayoutServerLoad } from './$types';
+
+export const load = (async ({fetch}) => {
+    return {
+        navigation: await (await fetch('http://localhost:1337/api/navigation/render/1', {
+			headers: {
+				'Authorization': `bearer ${STRAPI_API_TOKEN}`
+			}
+		})).json()
+    };
+}) satisfies LayoutServerLoad;
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+{JSON.stringify(data.navigation)}
+
 <h1>Welcome to SvelteKit</h1>
 <p>Read <a href="./posts">posts</a></p>
