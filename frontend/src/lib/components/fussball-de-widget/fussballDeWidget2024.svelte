51ad1e5ac89e0b43f0dc76e204b18bbe3cb1f5d6
Philipp Schardt <philipp.schardt@ergosign.de>
2024-09-01 10:56:24 +0200

    fix(widget): fussball.de iframe height

--- frontend/src/lib/components/fussball-de-widget/fussballDeWidget2024.svelte
+++ frontend/src/lib/components/fussball-de-widget/fussballDeWidget2024.svelte
@@ -1,7 +1,8 @@
 <script lang="ts" context="module">
 	function generateSecureRandomId(length: number) {
 		const randomValues = new Uint32Array(length);
-		window.crypto.getRandomValues(randomValues);
+
+		crypto.getRandomValues(randomValues);
 
 		const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 		let result = '';
@@ -10,6 +11,14 @@
 			result += characters[randomIndex];
 		}
 		return result;
+	}
+
+	interface IframeMessage extends Event {
+		data: {
+			type: string;
+			iframeName: string;
+			height: number;
+		};
 	}
 </script>
 
@@ -21,13 +30,30 @@
 	export let widgetId: string;
 
 	export let widgetType = 'competition';
+
+	const iframeName = `${generateSecureRandomId(4)}_fussballde_widget-${widgetId}`;
+
+	let iframeHeight = 'auto';
+
+	/**
+	 * Listen for height of iframe
+	 * @param event
+	 */
+	const onMessage = (event: IframeMessage) => {
+		if (event.data.type === 'fussballde_widget:resize' && event.data.iframeName == iframeName) {
+			iframeHeight = `${event.data.height}px`;
+		}
+	};
 </script>
+
+<svelte:window on:message={onMessage} />
 
 <iframe
 	title={title || `fussball.de Widget ${widgetId}`}
 	src={`https://next.fussball.de/widget/${widgetType}/${widgetId}`}
-	name={`${generateSecureRandomId(4)}_fussballde_widget-${widgetId}`}
+	name={iframeName}
 	width="100%"
+	height={iframeHeight}
 	frameborder="0"
 	scrolling="no"
 ></iframe>
