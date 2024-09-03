dcace8cf397ca83c67fd0b2e228b4c90fb3e45aa
Philipp Schardt <philipp.schardt@ergosign.de>
2024-09-03 22:42:30 +0200

    refactor: update case sensitivity files

---
+++ frontend/src/lib/components/fussball-de-widget/FussballDeWidget.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	/**
+	 * Unique id of element wrapper.
+	 */
+	export let id: string = browser
+		? `fussball-de-id_${Date.now()}${Math.floor(Math.random() * 100)}`
+		: crypto.randomUUID();
+
+	/**
+	 * Fussball.de widget id.
+	 */
+	export let widgetId: string;
+
+	onMount(() => {
+		// es-lint-ignore @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		new fussballdeWidgetAPI().showWidget(id, widgetId);
+	});
+</script>
+
+<div class="fussball-de-widget">
+	<div {id} />
+</div>
+
+<style>
+	:global(iframe) {
+		width: 100%;
+	}
+</style>
---
+++ frontend/src/lib/components/fussball-de-widget/FussballDeWidget2024.svelte
@@ -0,0 +1,69 @@
+<script lang="ts" context="module">
+	function generateSecureRandomId(length: number) {
+		const randomValues = new Uint32Array(length);
+
+		crypto.getRandomValues(randomValues);
+
+		const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		let result = '';
+		for (let i = 0; i < length; i++) {
+			const randomIndex = randomValues[i] % characters.length;
+			result += characters[randomIndex];
+		}
+		return result;
+	}
+
+	interface IframeMessage extends Event {
+		data: {
+			type: string;
+			iframeName: string;
+			height: number;
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let title = '';
+	/**
+	 * Fussball.de widget id.
+	 */
+	export let widgetId: string;
+
+	export let widgetType = 'competition';
+
+	const iframeName = `${generateSecureRandomId(4)}_fussballde_widget-${widgetId}`;
+
+	let iframeHeight = 'auto';
+
+	/**
+	 * Listen for height of iframe
+	 * @param event
+	 */
+	const onMessage = (event: IframeMessage) => {
+		if (event.data.type === 'fussballde_widget:resize' && event.data.iframeName == iframeName) {
+			iframeHeight = `${event.data.height}px`;
+		}
+	};
+
+	let wait = false;
+
+	setTimeout(() => {
+		//workaround to draw iframe in correct height when reloading whole page
+		wait = true;
+	});
+</script>
+
+<svelte:window on:message={onMessage} />
+
+<!-- workaround to draw iframe in correct height when reloading whole page -->
+{#if wait}
+	<iframe
+		title={title || `fussball.de Widget ${widgetId}`}
+		src={`https://next.fussball.de/widget/${widgetType}/${widgetId}`}
+		name={iframeName}
+		width="100%"
+		height={iframeHeight}
+		frameborder="0"
+		scrolling="no"
+	></iframe>
+{/if}
