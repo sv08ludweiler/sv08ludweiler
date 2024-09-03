3f501729ed8c5d5e614856ce730045d5717a2eda
Philipp Schardt <philipp.schardt@ergosign.de>
2024-09-03 22:28:35 +0200

    chore(dependencies): updated

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-3d847cb5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.7r3ndn30s78"
+    "revision": "0.cjdm2nrkk38"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
