4a59f35b490d0b4b216192a31c3aaff2697decbf
Philipp Schardt <philipp.schardt@ergosign.de>
2025-01-26 12:55:00 +0100

    chore(dependencies): updated

--- frontend/src/lib/components/fupa/widget.js
+++ frontend/src/lib/components/fupa/widget.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 'use strict';
 (() => {
 	var Mp = Object.create;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	server: {
@@ -45,6 +46,7 @@ export default defineConfig({
 				],
 			},
 		}),
+		tailwindcss()
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
