763f5977a3aa80256404946a6b2b412d86df1837
Philipp Schardt <philipp.schardt@ergosign.de>
2025-04-28 19:27:59 +0200

    chore(dependencies): updated

--- cms/config/plugins.js
+++ cms/config/plugins.js
@@ -1,13 +1,25 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
+	upload: {
+		config: {
+			providerOptions: {
+				localServer: {
+					maxage: 31536000, // one year
+				},
+			},
+		},
+	},
+
 	'strapi-cache': {
 		enabled: true,
 		config: {
 			debug: false, // Enable debug logs
-			max: 1000, // Maximum number of items in the cache
+			max: 1000, // Maximum number of items in the cache (only for memory cache)
 			ttl: 1000 * 60 * 60, // Time to live for cache items (1 hour)
-			size: 1024 * 1024 * 1024, // Maximum size of the cache (1 GB)
-			allowStale: false, // Allow stale cache items
+			size: 1024 * 1024 * 1024, // Maximum size of the cache (1 GB) (only for memory cache)
+			allowStale: false, // Allow stale cache items (only for memory cache)
 			cacheableRoutes: [], // Caches routes which start with these paths (if empty array, all '/api' routes are cached)
+			provider: 'memory', // Cache provider ('memory' or 'redis')
+			// redisUrl: env('REDIS_URL', 'redis://localhost:6379'), // Redis URL (if using Redis)
 		},
 	},
 });
